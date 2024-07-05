be32be8d1714b0a9c0fda8fa60b3e9d357f90d78
adityasahu2 <adityaprasadsahu2017@gmail.com>
2024-07-05 17:31:09 +0530

    First Commit

---
+++ Flex JQuery.js
@@ -0,0 +1,104 @@
+$(document).ready(function() {
+    $("#JC1").click(function() {
+        $(".boxes").css("justify-content","start")
+    })
+    $("#JC2").click(function() {
+        $(".boxes").css("justify-content","end")
+    })
+    $("#JC3").click(function() {
+        $(".boxes").css("justify-content","flex-start")
+    })
+    $("#JC4").click(function() {
+        $(".boxes").css("justify-content","flex-end")
+    })
+    $("#JC5").click(function() {
+        $(".boxes").css("justify-content","center")
+    })
+    $("#JC6").click(function() {
+        $(".boxes").css("justify-content","left")
+    })
+    $("#JC7").click(function() {
+        $(".boxes").css("justify-content","right")
+    })
+    $("#JC8").click(function() {
+        $(".boxes").css("justify-content","space-around")
+    })
+    $("#JC9").click(function() {
+        $(".boxes").css("justify-content","space-between")
+    })
+    $("#JC10").click(function() {
+        $(".boxes").css("justify-content","space-evenly")
+    })
+    $("#AI1").click(function() {
+        $(".boxes").css("align-items","start")
+    })
+    $("#AI2").click(function() {
+        $(".boxes").css("align-items","end")
+    })
+    $("#AI3").click(function() {
+        $(".boxes").css("align-items","flex-start")
+    })
+    $("#AI4").click(function() {
+        $(".boxes").css("align-items","flex-end")
+    })
+    $("#AI5").click(function() {
+        $(".boxes").css("align-items","center")
+    })
+    $("#AI6").click(function() {
+        $(".boxes").css("align-items","self-start")
+    })
+    $("#AI7").click(function() {
+        $(".boxes").css("align-items","self-end")
+    })
+    $("#AI8").click(function() {
+        $(".boxes").css("align-items","stretch")
+    })
+    $("#AI9").click(function() {
+        $(".boxes").css("align-items","baseline")
+    })
+    $("#AI10").click(function() {
+        $(".boxes").css("align-items","normal")
+    })
+    $("#FW1").click(function() {
+        $(".boxes").css("flex-wrap","wrap")
+    })
+    $("#FW2").click(function() {
+        $(".boxes").css("flex-wrap","nowrap")
+    })
+    $("#FW3").click(function() {
+        $(".boxes").css("flex-wrap","wrap-reverse")
+    })
+    $("#AC1").click(function() {
+        $(".boxes").css("align-content","start")
+    })
+    $("#AC2").click(function() {
+        $(".boxes").css("align-content","end")
+    })
+    $("#AC3").click(function() {
+        $(".boxes").css("align-content","flex-start")
+    })
+    $("#AC4").click(function() {
+        $(".boxes").css("align-content","flex-end")
+    })
+    $("#AC5").click(function() {
+        $(".boxes").css("align-content","center")
+    })
+    $("#AC6").click(function() {
+        $(".boxes").css("align-content","space-around")
+    })
+    $("#AC7").click(function() {
+        $(".boxes").css("align-content","space-between")
+    })
+    $("#AC8").click(function() {
+        $(".boxes").css("align-content","space-evenly")
+    })
+    $("#AC9").click(function() {
+        $(".boxes").css("align-content","stretch")
+    })
+    $("#AC10").click(function() {
+        $(".boxes").css("align-content","baseline")
+    })
+    $("#AC11").click(function() {
+        $(".boxes").css("align-content","normal")
+    })
+})
